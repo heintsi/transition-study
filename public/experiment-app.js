2fd7e12cd8ec0f35e06323c930cd05afb39f84a8
Hannu Arvila <hannu.arvila@reaktor.fi>
2014-11-18 16:04:30 +0200

    Add survey state handling

--- public/experiment-app.js
+++ public/experiment-app.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-  console.log('Alive and kicking!')
 
-  var durations = {
-    modal: 1000
+  function easing() { return $el.controlPanel().find('.easing-radio:checked').attr('value') }
+  function duration() {
+    var $slider = $el.controlPanel().find('.slider-animation')
+    return $slider.attr('max') - $slider.val()
   }
 
-  var easing = function() { return $el.controlPanel().find('.easing-radio:checked').attr('value') }
-
-  var activityLog = ['first-entry']
+  var activityLog = []
 
   var $el = {
     app: function() { return $('body') },
@@ -16,7 +15,42 @@ $(document).ready(function() {
     controlPanel: function() { return $('.control-panel') }
   }
 
-  renderImageModalSurvey()
+  function switchSurveyStateTo(stateName) {
+    var stateIndex = _.findIndex(surveyStates, { name: stateName });
+    surveyStates[stateIndex].render()
+    changeAnimationDurationsOnAdjust()
+    renderSurveyState()
+    bindSurveyStateSwitchers()
+    logActivity('state_switch')
+
+    function renderSurveyState() {
+      $el.surveyState().html(templatesObj['survey-state']({ currentStateNumber: stateIndex + 1, totalStatesCount: surveyStates.length }))
+      $el.surveyState().data('stateId', stateName)
+    }
+
+    function bindSurveyStateSwitchers() {
+      $el.surveyState().find('.previous-state-btn').click(function () {
+        switchSurveyStateTo(getPreviousSurveyStateName())
+      })
+      $el.surveyState().find('.next-state-btn').click(function () {
+        switchSurveyStateTo(getNextSurveyStateName())
+      })
+    }
+  }
+
+  function getSurveyState() { return $el.surveyState().data('stateId') }
+
+  function getPreviousSurveyStateName() { return surveyStates[Math.max(0, _.findIndex(surveyStates, { name: getSurveyState() }) - 1)].name }
+  function getNextSurveyStateName()     { return surveyStates[Math.min(surveyStates.length - 1, _.findIndex(surveyStates, { name: getSurveyState() }) + 1)].name }
+
+  var surveyStates = [
+    { name: 'modalSurvey',        render: renderImageModalSurvey },
+    { name: 'insertionSurvey',    render: renderInsertionSurvey },
+    { name: 'deletionSurvey',     render: renderDeletionSurvey },
+    { name: 'screenChangeSurvey', render: renderScreenChangeSurvey }
+  ]
+
+  switchSurveyStateTo('modalSurvey')
 
   function renderImageModalSurvey() {
     renderControlPanel({
@@ -27,11 +61,81 @@ $(document).ready(function() {
     })
     renderImageThumbnails()
     openImageModalOnClick()
-    changeAnimationDurationsOnAdjust()
+
+    function openImageModalOnClick() {
+      var modalBgOverlay = $el.app().find('.image-modal-bg-overlay'),
+        imageModal = $el.app().find('.image-product__flying-thumbnail')
+      $el.surveyContent().find('.image-product__thumbnail').click(function (e) {
+
+        logActivity('modal_click', 'open')
+
+        modalBgOverlay.velocity('fadeIn', {
+          queue: false,
+          duration: duration(),
+          easing: easing()
+        })
+        imageModal.show()
+          .velocity({
+            scale: 5,
+            top: '180px'
+          }, {
+            queue: false,
+            display: 'block',
+            duration: duration(),
+            easing: easing()
+          })
+
+      })
+      imageModal.click(function () {
+        logActivity('modal_click', 'close')
+        modalBgOverlay.velocity('fadeOut', {
+          queue: false,
+          duration: duration(),
+          easing: easing()
+        })
+        imageModal
+          .velocity({
+            scale: 1,
+            top: '0'
+          }, {
+            display: 'none',
+            queue: false,
+            duration: duration(),
+            easing: easing()
+          })
+      })
+    }
+  }
+
+  function renderInsertionSurvey() {
+    renderControlPanel({
+      taskHeader: 'Kuvan lisääminen kokoelmaan',
+      taskDescription: 'Klikkaa oikealla olevaa kuvaa, jolloin kuva lisätään kokoelmaan. Säädä animaatio sopivaksi.',
+      functionalityDescription: 'Animaation nopeus',
+      sliderType: 'add'
+    })
+    renderContentArea()
+    addImageToCollectionOnClick()
+
+    function addImageToCollectionOnClick() {
+
+    }
+  }
+
+  function renderDeletionSurvey() {
+
+  }
+
+  function renderScreenChangeSurvey() {
+
   }
 
   function renderControlPanel(context) {
     $el.controlPanel().html(templatesObj['control-panel'](context))
+  }
+
+  function renderContentArea(templateName) {
+
   }
 
   function renderImageThumbnails() {
@@ -45,76 +149,18 @@ $(document).ready(function() {
     $el.surveyContent().html(templatesObj['experiment_image-modal'](context))
   }
 
-  function openImageModalOnClick() {
-    var modalBgOverlay = $el.app().find('.image-modal-bg-overlay'),
-      imageModal = $el.app().find('.image-product__flying-thumbnail')
-    $el.surveyContent().find('.image-product__thumbnail').click(function(e) {
-
-      logActivity('modal clicked', 'open')
-
-      modalBgOverlay.velocity('fadeIn', {
-        queue: false,
-        duration: modalDuration(),
-        easing: easing()
-      })
-      imageModal.show()
-        .velocity({
-          scale: 5,
-          top: '180px'
-        }, {
-          queue: false,
-          display: 'block',
-          duration: modalDuration(),
-          easing: easing()
-        })
-
-    })
-    imageModal.click(function () {
-      logActivity('modal clicked', 'close')
-      modalBgOverlay.velocity('fadeOut', {
-        queue: false,
-        duration: modalDuration(),
-        easing: easing()
-      })
-      imageModal
-        .velocity({
-          scale: 1,
-          top: '0'
-        }, {
-          display: 'none',
-          queue: false,
-          duration: modalDuration(),
-          easing: easing()
-        })
-    })
-  }
-
-  function modalDuration() {
-    return durations.modal
-  }
-
-  function animDuration(type) {
-    return durations[type]
-  }
-
   function changeAnimationDurationsOnAdjust() {
-    $el.app().find('.control-panel .slider-animation')
-    var $slider = $el.app().find('.control-panel .slider-animation')
-    var sliderType = $slider.attr('name')
-    $slider.val(animDuration(sliderType))
-    $slider.change(_.throttle(function() {
-      var sliderVal = 2000 - $slider.val() // is this perhaps more intuitive?
-      durations[sliderType] = Number(sliderVal)
-      logActivity('animation value changed to', sliderVal)
+    $el.controlPanel().find('.slider-animation').change(_.throttle(function() {
+      logActivity('anim_change', duration())
     }, 200))
-
   }
 
   function logActivity(msg, value) {
     activityLog.push({
       timestamp: new Date().getTime(),
-      message: msg,
-      value: value
+      state: getSurveyState(),
+      msg: msg,
+      val: value
     })
   }
 
